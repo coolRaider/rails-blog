04c0fc4b732caa44e78c860993b194586565208e
Tanay Upadhyaya <tanayupadhyaya@gmail.com>
2016-06-24 17:24:35 +0530

    Fixing indentation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,13 +21,13 @@ Rails.application.configure do
 
   #gmail settings
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
 
   # Print deprecation notices to the Rails logger.
